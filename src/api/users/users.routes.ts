0c71a4cedd82d22cb79caed750f5000d4141703c
Julianne Adams <julianne@julianneadams.info>
2022-05-09 16:56:27 -0400

    Add: placeholders for users routes

--- src/api/users/users.routes.ts
+++ src/api/users/users.routes.ts
@@ -24,13 +24,15 @@ import controller from "./usersController";
  *           description: The user's username
  *         email:
  *           type: string
+ *           format: email
  *           description: The user's email address
  *         password:
  *           type: string
+ *           format: password
  *           description: The user's hashed password
  *         joinDate:
  *           type: string
- *           format: date
+ *           format: date-time
  *           description: The date and time that the user joined
  *         posts:
  *           type: array
@@ -42,6 +44,14 @@ import controller from "./usersController";
  *           description: Array of IDs of the user's threads
  *           items:
  *             type: string
+ *   requestBodies:
+ *     UserBody:
+ *       description: A JSON object containing user information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/User"
  * tags:
  *   name: Users
  *   description: Operations on user accounts
@@ -91,5 +101,76 @@ router.get("/", controller.getUsers);
  *         description: User cannot be found
  */
 router.get("/:id", controller.getUserById);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   patch:
+ *     summary: Update a user's information
+ *     tags: [Users]
+ *     parameters:
+ *       - in : path
+ *         name: id
+ *         description: id of the user
+ *         schema:
+ *           type: string
+ *           example: 12cew34d224r7d
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: The user was updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User updated successfully
+ */
+// TODO: document request body
+router.patch("/:id", controller.updateUser);
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       $ref: "#/components/requestBodies/UserBody"
+ *     responses:
+ *       201:
+ *         description: The user was created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User created successfully
+ */
+router.post("/", controller.createUser);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The user was deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User deleted successfully
+ */
+router.delete("/:id", controller.deleteUser);
 
 export default router;
--- src/api/users/usersController.ts
+++ src/api/users/usersController.ts
@@ -42,7 +42,33 @@ const getUserById = (req: Request, res: Response, next: Function) => {
   return user ? res.status(200).json({ user }) : res.status(400);
 };
 
+/**
+ * Update a user's information
+ *
+ * @returns If successful, returns 200 status and a message indicating completion
+ */
+const updateUser = (req: Request, res: Response, next: Function) => {
+  return res.status(200).json({ message: "Updating user..." });
+};
+
+/**
+ * Create a new user account
+ */
+const createUser = (req: Request, res: Response, next: Function) => {
+  return res.status(200).json({ message: "Creating user..." });
+};
+
+/**
+ * Delete a user
+ */
+const deleteUser = (req: Request, res: Response, next: Function) => {
+  return res.status(200).json({ message: "Deleting user..." });
+};
+
 export default {
   getUsers,
   getUserById,
+  updateUser,
+  createUser,
+  deleteUser,
 };
