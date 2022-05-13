5cb8bbc6c7aa60198688767655d597854e42b4b4
Julianne Adams <julianne@julianneadams.info>
2022-05-12 21:42:06 -0400

    Add: more tests for placeholder routes

--- src/api/users/users.routes.test.ts
+++ src/api/users/users.routes.test.ts
@@ -2,22 +2,21 @@ import request from "supertest";
 import app from "../../app";
 
 describe("Test the routes at /users", () => {
+  const responseMessage = expect.objectContaining({
+    message: expect.any(String),
+  });
+
+  const userObject = expect.objectContaining({
+    _id: expect.any(String),
+    email: expect.any(String),
+    password: expect.any(String),
+    posts: expect.any(Array),
+    threads: expect.any(Array),
+    joinDate: expect.any(String),
+  });
   [
     describe("the endpoint /users", () => {
-      const userObject = expect.objectContaining({
-        _id: expect.any(String),
-        email: expect.any(String),
-        password: expect.any(String),
-        posts: expect.any(Array),
-        threads: expect.any(Array),
-        joinDate: expect.any(String),
-      });
-
       const usersArray = expect.arrayContaining([userObject]);
-
-      const responseMessage = expect.objectContaining({
-        message: expect.any(String),
-      });
 
       it("should respond to GET requests by returning 200 and a list of user objects", async () => {
         const response: request.Response = await request(app).get("/users");
@@ -30,6 +29,31 @@ describe("Test the routes at /users", () => {
       it("should respond to POST requests with 201 and a confirmation message upon success", async () => {
         const response: request.Response = await request(app).post("/users");
         expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual(responseMessage);
+      });
+    }),
+    describe("the endpoint /users/{id}", () => {
+      it("should respond to GET requests by returning 200 and a user object", async () => {
+        const response: request.Response = await request(app).get("/users/123");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(
+          expect.objectContaining({ user: userObject }),
+        );
+      });
+
+      it("should respond to PATCH requests by returning 200 and a confirmation message upon success", async () => {
+        const response: request.Response = await request(app).patch(
+          "/users/123",
+        );
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(responseMessage);
+      });
+
+      it("should respond to DELETE requests by returning 200 and a confirmation message upon success", async () => {
+        const response: request.Response = await request(app).delete(
+          "/users/123",
+        );
+        expect(response.statusCode).toBe(200);
         expect(response.body).toEqual(responseMessage);
       });
     }),
