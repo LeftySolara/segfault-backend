62d0b2bcc493a3f313a5675e25b94b745aaf6a94
Julianne Adams <julianne@julianneadams.info>
2022-05-13 16:40:19 -0400

    Add: placeholder functions for boardCategories controller

---
+++ src/api/boardCategories/boardCategories.controller.test.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import controller from "./boardCategories.controller";
+
+describe("The boardCategories controller", () => {
+  const req = {};
+  const mockResponse = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown;
+
+  describe("getCategories", () => {
+    it("should return 200 and a confirmation message", () => {
+      controller.getCategories(
+        req as Request,
+        mockResponse as Response,
+        jest.fn(),
+      );
+      const mRes = mockResponse as Response;
+      expect(mRes.status).toBeCalledWith(200);
+      expect(mRes.json).toBeCalledWith({ message: expect.any(String) });
+    });
+  });
+
+  describe("getCategoryById", () => {
+    it("should return 200 and a confirmation message", () => {
+      controller.getCategoryById(
+        req as Request,
+        mockResponse as Response,
+        jest.fn(),
+      );
+      const mRes = mockResponse as Response;
+      expect(mRes.status).toBeCalledWith(200);
+      expect(mRes.json).toBeCalledWith({ message: expect.any(String) });
+    });
+  });
+
+  describe("updateCategory", () => {
+    it("should return 200 and a confirmation message", () => {
+      controller.updateCategory(
+        req as Request,
+        mockResponse as Response,
+        jest.fn(),
+      );
+      const mRes = mockResponse as Response;
+      expect(mRes.status).toBeCalledWith(200);
+      expect(mRes.json).toBeCalledWith({ message: expect.any(String) });
+    });
+  });
+
+  describe("createCategory", () => {
+    it("should return 201 and a confirmation message", () => {
+      controller.createCategory(
+        req as Request,
+        mockResponse as Response,
+        jest.fn(),
+      );
+      const mRes = mockResponse as Response;
+      expect(mRes.status).toBeCalledWith(201);
+      expect(mRes.json).toBeCalledWith({ message: expect.any(String) });
+    });
+  });
+
+  describe("deleteCategory", () => {
+    it("should return 200 and a confirmation message", () => {
+      controller.deleteCategory(
+        req as Request,
+        mockResponse as Response,
+        jest.fn(),
+      );
+      const mRes = mockResponse as Response;
+      expect(mRes.status).toBeCalledWith(200);
+      expect(mRes.json).toBeCalledWith({ message: expect.any(String) });
+    });
+  });
+});
