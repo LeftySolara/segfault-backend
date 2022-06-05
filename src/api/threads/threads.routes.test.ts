5f9d37315e7813edd199550f99bf2295a25d970f
Julianne Adams <julianne@julianneadams.info>
2022-06-05 11:38:32 -0400

    Add: test for GET /threads route

--- src/api/threads/threads.routes.test.ts
+++ src/api/threads/threads.routes.test.ts
@@ -11,10 +11,60 @@ describe("Test the routes at /threads", () => {
   testHelpers.routeTestInit(app);
 
   describe("the endpoint /threads", () => {
-    it("should respond to GET requests by returning 200 and a confirmation message", async () => {
-      const response: request.Response = await request(app).get("/threads");
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual(responseMessage);
+    describe("for GET requests", () => {
+      it("should respond with 200 and an array of thread objects", async () => {
+        const username = "getTest";
+        const email = "getTest@example.com";
+        const authorId = await testHelpers.generateUserId(
+          username,
+          email,
+          "password123!",
+        );
+
+        const boardCategoryTopic = "GET Test";
+        const boardCategoryId = await testHelpers.generateCategoryId(
+          boardCategoryTopic,
+        );
+
+        const boardTopic = "GET Test";
+        const boardId = await testHelpers.generateBoardId(
+          boardTopic,
+          "Description",
+          boardCategoryId,
+        );
+
+        const threadTopic = "GET Test";
+        await request(app).post("/threads").send({
+          authorId,
+          boardId,
+          topic: threadTopic,
+        });
+
+        const response: request.Response = await request(app).get("/threads");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+          threads: [
+            {
+              __v: expect.any(Number),
+              _id: expect.any(String),
+              id: expect.any(String),
+              author: {
+                authorId,
+                email,
+                username,
+              },
+              board: {
+                boardId,
+                topic: boardTopic,
+              },
+              lastPost: null,
+              posts: expect.any(Array),
+              topic: threadTopic,
+              dateCreated: expect.any(String),
+            },
+          ],
+        });
+      });
     });
 
     describe("for POST requests", () => {
