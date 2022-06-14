cad3b9186e8e3b7354e1f4167ea3fad08d85a1c6
Julianne Adams <julianne@julianneadams.info>
2022-06-14 15:21:46 -0400

    Add: functions for creating and updating posts

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
 import PostModel from "../models/post";
+import ThreadModel from "../models/thread";
+import UserModel from "../models/user";
 import HttpError from "../utils/httpError";
 
 /**
@@ -84,4 +87,108 @@ const getByThread = async (id: string) => {
   return posts.map((post) => post.toObject({ getters: true }));
 };
 
-export default { getAll, getById, getByUser, getByThread };
+/**
+ * Create a new post in a thread
+ *
+ * @param {string} authorId - The id of the creator of the post
+ * @param {string} threadId - The thread the post belongs to
+ * @param {string} content - The text content of the post
+ *
+ * @throws after a database error
+ * @throws if the author does not exist
+ * @throws if the thread does not exist
+ *
+ * @returns An object containing post information
+ */
+const create = async (authorId: string, threadId: string, content: string) => {
+  // Check that the thread exists
+  let thread;
+  try {
+    thread = await ThreadModel.findById(threadId);
+  } catch (err: unknown) {
+    throw new HttpError("Error creating post", 500);
+  }
+  if (!thread) {
+    throw new HttpError("Cannot find thread", 404);
+  }
+
+  // Check that the author exists
+  let author;
+  try {
+    author = await UserModel.findById(authorId);
+  } catch (err: unknown) {
+    throw new HttpError("Error creating post", 500);
+  }
+  if (!author) {
+    throw new HttpError("Cannot find user", 404);
+  }
+
+  // Create the post
+  const post = new PostModel({
+    author: {
+      authorId: author._id,
+      username: author.username,
+      email: author.email,
+    },
+    thread: {
+      threadId: thread._id,
+      topic: thread.topic,
+    },
+    dateCreated: Date.now(),
+    content,
+  });
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+
+    await post.save({ session: sess });
+
+    author.posts.push(post._id);
+    await author.save({ session: sess, validateModifiedOnly: true });
+
+    thread.posts.push(post._id);
+    await thread.save({ session: sess, validateModifiedOnly: true });
+
+    sess.commitTransaction();
+  } catch (err: unknown) {
+    throw new HttpError("Error saving thread", 500);
+  }
+
+  return post.toObject({ getters: true });
+};
+
+/**
+ * Update a post's content
+ *
+ * @param {string} id - The id of the post to update
+ * @param {string} content - The new content of the post
+ *
+ * @throws after a database error
+ * @throws if the post cannot be found
+ *
+ * @returns An object containing updated post information
+ */
+const update = async (id: string, content: string) => {
+  let post;
+
+  try {
+    post = await PostModel.findById(id);
+  } catch (err: unknown) {
+    throw new HttpError("Error fetching post", 500);
+  }
+  if (!post) {
+    throw new HttpError("Cannot find post", 404);
+  }
+
+  try {
+    post.content = content;
+    await post.save();
+  } catch (err: unknown) {
+    throw new HttpError("Error saving post", 500);
+  }
+
+  return post.toObject({ getters: true });
+};
+
+export default { getAll, getById, getByUser, getByThread, create, update };
