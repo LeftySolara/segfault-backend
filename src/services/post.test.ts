3064fad84fb1106c78c68182868fd1c5dc41b82d
Julianne Adams <julianne@julianneadams.info>
2022-06-15 12:29:17 -0400

    Add: tests for create() in post service

--- src/services/post.test.ts
+++ src/services/post.test.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostService from "./post";
 import testHelpers from "../utils/testHelpers";
 
@@ -63,6 +64,53 @@ describe("The Post service", () => {
       const thread = await testHelpers.generateThread();
       const posts = await PostService.getByThread(thread.id);
       expect(posts).toEqual([]);
+    });
+  });
+
+  describe("create", () => {
+    it("should return an object containing post information", async () => {
+      const thread = await testHelpers.generateThread();
+      const user = await testHelpers.generateUser();
+      const content = "Post content";
+
+      const post = await PostService.create(user.userId, thread.id, content);
+
+      expect(post).toEqual({
+        __v: expect.any(Number),
+        _id: expect.any(mongoose.Types.ObjectId),
+        author: {
+          authorId: new mongoose.Types.ObjectId(user.userId),
+          email: user.email,
+          username: user.username,
+        },
+        thread: {
+          threadId: new mongoose.Types.ObjectId(thread.id),
+          topic: thread.topic,
+        },
+        content,
+        dateCreated: expect.any(Date),
+        id: expect.any(String),
+      });
+    });
+
+    it("should throw an error if the thread does not exist", async () => {
+      const user = await testHelpers.generateUser();
+      const content = "Post content";
+
+      expect(
+        async () =>
+          await PostService.create(user.userId, "123456789012", content),
+      ).rejects.toThrow();
+    });
+
+    it("should throw an error if the author does not exist", async () => {
+      const thread = await testHelpers.generateThread();
+      const content = "Post content";
+
+      expect(
+        async () =>
+          await PostService.create("123456789012", thread.id, content),
+      ).rejects.toThrow();
     });
   });
 });
