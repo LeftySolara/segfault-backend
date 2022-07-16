aab6a11a3bc50fd9a64fa75911b009a4d60e446e
Julianne Adams <julianne@julianneadams.info>
2022-07-16 13:21:54 -0400

    Add: cross-origin headers

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -10,6 +10,17 @@ export default async ({ app }: { app: Application }) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  // add CORS headers to all responses
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+  });
+
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(config.swagger));
 
   app.use("/users", api.users);
